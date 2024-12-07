3d271628655844a237300bd74b2a683caaf79ab2
Shubham Kumar <shubhamsre1997@gmail.com>
2024-12-07 17:48:44 +0530

    Write the first test case

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""
+This is a simple calculator app that can add, subtract, multiply, and divide.
+"""
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+This is a simple test for the calculator app.
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_number(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15) 
+        self.assertEqual(res, -5)
