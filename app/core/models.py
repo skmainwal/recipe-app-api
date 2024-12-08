5fec0cb4429697afc133875488755bd0b16e2097
Shubham Kumar <shubhamsre1997@gmail.com>
2024-12-08 13:23:24 +0530

    fixed the lint issues

--- app/core/models.py
+++ app/core/models.py
@@ -1,23 +1,29 @@
 """
 Database models.
 """
-from django.db import models  # Importing Django's models module to define database models
+# Importing Django's models module to define database models
+from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,  # Base class for creating a custom user model
     BaseUserManager,   # Base class for creating a custom user manager
-    PermissionsMixin,   # Mixin that adds permission-related fields and methods
+    PermissionsMixin,  # Mixin that adds permission-related fields and methods
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError('User must have an email address.')    # Raise an error if email is not provided
+            raise ValueError(
+                'User must have an email address.'
+            )  # Raise an error if email is not provided
         # Normalize the email address by lowercasing the domain part of it
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
         # Set the user's password using Django's built-in method
         user.set_password(password)
         # Save the user instance to the database using the specified database alias
