0438f68941b3f610cee02abd380a29884e3ce334
Shubham Kumar <shubhamsre1997@gmail.com>
2025-07-04 17:09:49 +0530

    Update and Delete Ingredient Test case

--- app/core/tests/test_ingredients_api.py
+++ app/core/tests/test_ingredients_api.py
@@ -14,6 +14,10 @@ from recipe.serializers import IngredientSerializer
 
 INGREDIENT_URL = reverse('recipe:ingredient-list')
 
+def detail_url(ingredient_id):
+    """Return ingredient detail URL."""
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])    
+
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a test user."""
@@ -67,11 +71,22 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data[0]['name'], ingredient.name)
         self.assertEqual(res.data[0]['id'], ingredient.id)
 
-    # def test_update_ingredient(self):
-    #     """Test updating an ingredient."""
-    #     ingredient = Ingredient.objects.create(user=self.user, name='Ingredient1')
-    #     payload = {'name': 'Ingredient2'}
-    #     url = detail_url(ingredient.id)
-    #     res = self.client.patch(url, payload)
-    #     self.assertEqual(res.status_code, status.HTTP_200_OK)
-    #     ingredient.refresh_from_db()
+    def test_update_ingredient(self):
+        """Test updating an ingredient."""
+        ingredient = Ingredient.objects.create(user=self.user, name='Cilantro')
+        payload = {'name': 'Coriander'}
+        url = detail_url(ingredient.id)
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        ingredient.refresh_from_db()
+
+        self.assertEqual(ingredient.name, payload['name'])
+
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient."""
+        ingredient = Ingredient.objects.create(user=self.user, name='Lettuce')
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())  
